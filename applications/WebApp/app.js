8528a6ce2a9a83782c70e54ddff8a393c323af18
Jayson Pan <jaysonpan11@gmail.com>
2019-02-21 12:57:49 -0500

    Remove mongoose

--- applications/WebApp/app.js
+++ applications/WebApp/app.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var session = require('express-session');
-var mongoose = require('mongoose');
 
 var app = express();
 app.use(express.static(__dirname + '/'));
