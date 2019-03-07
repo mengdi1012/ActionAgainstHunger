b10fb169cb4010387f9906c6617a3bd1014242e3
mephistoshadow <siqi.yang@mail.utoronto.ca>
2019-03-07 18:28:40 -0500

    add the js file to the Global_pst page and create the sample post page

---
+++ applications/WebApp/assets/js/Global_post.js
@@ -0,0 +1,100 @@
+
+
+// here is the part using the jquery.
+$(document).ready(function(){
+    $(".title").click(function(){
+
+      let title = $(".title").text()
+      console.log(title);
+      let full_name = "Post_" + title + ".html"
+      window.open(full_name);
+    });
+
+	$("#search").click(function(){
+		let title = $("#search_area").val()
+		let full_name = "Post_" + title + ".html"
+    	window.open(full_name);
+  	});
+
+  	
+});
+
+// get all the element that I needed 
+const newpost = document.getElementById("refresh");
+const profile =  document.getElementById("status");
+const row = document.getElementById("row");
+const home = document.getElementById("home");
+
+
+newpost.addEventListener('click', add_post);
+profile.addEventListener('click', go_profile);
+home.addEventListener('click', go_home_page);
+
+
+
+/// below is all the classes used
+class post{
+  constructor(id, title, description, date) {
+    this.id = id;
+    this.title = title;
+    this.description = description;
+    this.date = date;
+  }
+}
+
+
+
+// below is the all the function using vallina js.
+
+
+// add post into global page
+function add_post(e) {
+  e.preventDefault();
+  const  id = "d";
+  const title = "2";
+  const description = "3"
+  var dateObj = new Date();
+  var month = dateObj.getUTCMonth() + 1; //months from 1-12
+  var day = dateObj.getUTCDate();
+  var year = dateObj.getUTCFullYear();
+
+  newdate = year + "/" + month + "/" + day;
+  post_new = new post(id, title, description, newdate);
+  const new_p = row.children[0].cloneNode(true);
+  new_p.children[0].children[0].innerText = "2";
+  new_p.children[1].innerText = "3";
+  new_p.children[2].innerText = newdate;
+  row.appendChild(new_p);
+}
+
+//assign the profile button and determine the user type to jump to the certian pages.
+
+function go_profile(e) {
+   e.preventDefault();
+   const type = "student";
+   if (type == "student") {
+     const name = "student-profile.html"
+      window.open(name)
+   }else if (type == "teacher") {
+    const name = "teacher-profile.html"
+      window.open(name)
+   }else if (type == "professional") {
+    // const name = "teacher-profile.html"
+    //   window.open(name)
+   }
+}
+
+
+function go_home_page(e) {
+  e.preventDefault();
+   window.open("");
+}
+
+
+
+
+
+
+
+
+
