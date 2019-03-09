632919edcee393c3d8c4a7efbb762715f87a6994
mephistoshadow <siqi.yang@mail.utoronto.ca>
2019-03-08 21:52:52 -0500

    add the js file to the professional page

---
+++ applications/WebApp/assets/js/professional_post.js
@@ -0,0 +1,58 @@
+
+// class post{
+//   constructor(id, title, description, date) {
+//     this.id = id;
+//     this.title = title;
+//     this.description = description;
+//     this.date = date;
+//   }
+// }
+
+// var post = Class.extend({
+//   init: function(id,title,description,date){
+//    this.id = id;
+//     this.title = title;
+//     this.description = description;
+//     this.date = date;
+//   },
+// });
+
+
+post = function(id,title,description,date) {
+  this.init(id);
+  this.init(title);
+   this.init(description);
+   this.init(date);
+}
+
+$.extend(post.prototype, {
+   // object variables
+ 
+
+   init: function(id,title,description,date) {
+     // do initialization here
+      this.id = id;
+      this.title = title;
+      this.description = description;
+      this.date = date;
+   },
+
+
+});
+
+
+
+$("#submit").click(function(e){
+	  e.preventDefault();
+      let type = $("#type_c").val()
+      let title =  $("#type_t").val()
+      let content =  $("#content").val()
+      const id = 123
+      const date = new Date();
+      let post_new = new post(id,title,content,date)
+      console.log(date);
+    });
+
+
+
+
