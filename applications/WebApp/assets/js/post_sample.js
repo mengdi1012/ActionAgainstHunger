7366e2ea3f51ec333131a87645e2009b7b358d9b
mephistoshadow <siqi.yang@mail.utoronto.ca>
2019-03-10 00:43:32 -0500

    add the exchange value part to post_sample page

--- applications/WebApp/assets/js/post_sample.js
+++ applications/WebApp/assets/js/post_sample.js
@@ -2,23 +2,46 @@ $("#submit").click(function(e){
       e.preventDefault() 
       let clone_one = $("#clone").clone();
       let index = clone_one.children().children();
-      let name = "";
-      let school = "";
-      name = req.session.name;
-      console.log(name);
+      let name = "123123";
+      let school = "School: ";
+      let content = "";
        $.ajax({
-        url: '/post/LzTOfcoh0W9H4dcYA5JX',
+        url: '/user',
         type: 'GET',
         dataType: 'json', // added data type
         success: function(data) {
+        	name = data.username;
+        	school = school + data.school;
          
         }
     });
-      index.children()[1].text;
-      ndex.children()[2].text;
+      index.children()[1].text = name;
+      index.children()[2].text  = school;
+      console.log();
+      content =  $("#comment_a").val(); 
+      index[2].text = content;
 
+
+
+      let comment = {
+      	author : name,
+      	schoold : school,
+      	content : content
+      }
 
       clone_one.insertBefore("#comment_box");
+
+      $.ajax({
+        url: '/post/:postId',
+        type: 'Post',
+       	dataType: "json",
+        contentType: "application/json; charset=utf-8",
+        data: JSON.stringify(comment),
+        success: function(response) {
+        window.alert('success');
+        }
+    });
+
     });
 
 
