e026dc3c4388b7aed3e66e3ca70f95fffc9c3242
jayson <jaysonpan11@gmail.com>
2019-03-31 04:26:54 -0400

    extension profile page for different logged in user

--- applications/Frontend/action-against-hunger/src/app/user/profile/profile-activity/profile-activity.component.ts
+++ applications/Frontend/action-against-hunger/src/app/user/profile/profile-activity/profile-activity.component.ts
@@ -1,6 +1,9 @@
 import {Component, OnInit} from '@angular/core';
 import { AuthService } from '../../../service/auth.service';
+import { User } from '../../../model/user.model';
 import {Router} from '@angular/router';
+import { UsersService } from '../../../service/users.service';
+
 
 @Component({
   selector: 'app-profile-activity',
@@ -8,12 +11,31 @@ import {Router} from '@angular/router';
   styleUrls: ['./profile-activity.component.css']
 })
 export class ProfileActivityComponent implements OnInit {
+  isTeacher = false;
+  user: User;
 
-  constructor(private authService: AuthService, private router: Router) {
+
+  constructor(private authService: AuthService, private router: Router, private userService: UsersService) {
     console.log('Creating activity Component');
   }
   ngOnInit() {
+    this.getUser();
+  }
 
+  getUser(): void {
+    console.log("try get user info");
+    this.userService.getUserInfo()
+      .subscribe(user => {
+        this.user = user
+        console.log("get response:", user)
+        if(user["username"] != ""){
+          console.log("get user detail, ", user)
+          this.isTeacher = this.user.usertype == "teacher";
+        }else{
+          window.alert("not valid session");
+        }          
+      
+      });
   }
   goReset() {
     console.log("go to reset")
