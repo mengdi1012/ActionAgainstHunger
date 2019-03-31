656d627921a8432c782c9758b0fb5bc6076eddd0
mengdi shi <mengdishi@gmail.com>
2019-03-31 01:13:31 -0400

    build notification service

--- applications/Frontend/action-against-hunger/src/app/service/notification.service.ts
+++ applications/Frontend/action-against-hunger/src/app/service/notification.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NotificationService {
+  
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+    
+  getNotification(user: string): Observable<Comment[]>{
+    const url = environment.APIEndpoint + "/api/notification" + user;
+    return this.http.get<Comment[]>(url, {withCredentials: true});
+}
 }
