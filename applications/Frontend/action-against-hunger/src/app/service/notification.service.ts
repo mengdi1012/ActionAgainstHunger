14c51c98d241d1eb7a41b53c41aa0a433a16695d
jayson <jaysonpan11@gmail.com>
2019-03-31 21:10:20 -0400

    fix comment type issue

--- applications/Frontend/action-against-hunger/src/app/service/notification.service.ts
+++ applications/Frontend/action-against-hunger/src/app/service/notification.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { environment } from '../../environments/environment';
+import { Comment } from '../model/comment.model'
 
 @Injectable({
   providedIn: 'root'
